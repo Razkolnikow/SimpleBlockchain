91dfebb5e0de4095068802ba571787c3f54db1b3
Raskolnikov <hristov.pavel88@gmail.com>
2018-06-13 22:44:13 +0300

    started implementing the transaction class

--- Node/models/transaction.js
+++ Node/models/transaction.js
@@ -1,3 +1,16 @@
 module.exports = class Transaction {
-    // TODO
+    constructor () {
+        // TODO
+        this.from = ''; // Address (40 hex digits)
+        this.to = ''; // Address (40 hex digits)
+        this.value = ''; // non negative integer
+        this.fee = ''; //   non negative integer
+        this.dateCreated = ''; // String ISO8601_string
+        this.data = ''; // String optional
+        this.senderPubKey = ''; // hex number 65
+        this.transactionDataHash = ''; // Hex_number
+        this.senderSignature = ''; // hex number [2][64]
+        this.minedInBlockIndex = ''; // integer
+        this.transferSuccessful = ''; // bool
+    }
 }
