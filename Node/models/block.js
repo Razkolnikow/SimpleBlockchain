f67912716117423cff63f9b6dd3a1469666ea8c5
Raskolnikov <hristov.pavel88@gmail.com>
2018-06-13 22:34:47 +0300

    partially implemented the block class

--- Node/models/block.js
+++ Node/models/block.js
@@ -1,3 +1,28 @@
+const CryptoJS = require('crypto-js');
+
 module.exports = class Block {
-    // TODO
+    constructor () {
+        this.index = 0; // TODO Generate index
+        this.transactions= [];
+        this.difficulty = 4; // TODO
+        this.prevBlockHash = '';
+        this.minedBy = ''; // Miner address
+        this.blockDataHash = ''; // When sending the blockdatahash and the difficulty the node
+        // should send the index to the miner also, so the miner can check if there is newer blocks to mine
+
+        // Values received from the miner
+        this.nonce = 0;
+        this.dateCreated = '';
+        this.minedBlockHash = '';
+    }
+
+    calculateBlockHash() {
+        return CryptoJS.SHA256(
+            this.index +
+            this.prevBlockHash + 
+            this.difficulty +
+            JSON.stringify(this.transactions) +
+            this.minedBy
+        );
+    }
 }
