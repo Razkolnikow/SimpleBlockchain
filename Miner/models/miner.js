c658c3b2ffc2c5eeb906a2bedb25402d5f29301a
Raskolnikov <hristov.pavel88@gmail.com>
2018-06-15 10:43:52 +0300

    TODO comments in node and miner classes

--- Miner/models/miner.js
+++ Miner/models/miner.js
@@ -8,6 +8,11 @@ module.exports = class Miner {
         this.minerAddress = 'test address';
     }
 
+    // TODO: The miner should ask the node on every two seconds for example if there is a new block
+    // when the node returns bigger block index, the miner should leave the block that it is mining at the moment
+    // and asks for the new block hash !!!
+    // The miner should send the minedBlockHash with the block index!!!
+
     calculateHash(blockHash, difficulty) {
         while(this.hash.toString().substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.hash = CryptoJS.SHA256(blockHash + this.nonce + this.timestamp);
