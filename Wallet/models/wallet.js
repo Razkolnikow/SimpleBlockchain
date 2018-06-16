a679643a4d05f8908f5eb7499ab57152a7361a3b
Raskolnikov <hristov.pavel88@gmail.com>
2018-06-16 17:46:26 +0300

    testing wallet functions - implementation in process

--- Wallet/models/wallet.js
+++ Wallet/models/wallet.js
@@ -1,9 +1,73 @@
 const ethers = require('ethers');
+const CryptoJS = require('crypto-js');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const secp256k1 = require('secp256k1');
+const eu = require('ethereumjs-util');
 
 module.exports = class Wallet {
     constructor () {
 
     }
 
+    createRandomWallet() {
+        return new ethers.Wallet.createRandom();
+    }
+
+    saveWalletToJson(wallet, password) {
+        return wallet
+            .encrypt(password)
+            //.then(console.log);
+    }
+
+    getWalletFromJSON(json, password) {
+        return ethers.Wallet.fromEncryptedWallet(json, password);
+    }
+
+    generateMnemonic () {
+        let randomEntropyBytes = ethers.utils.randomBytes(16);
+        return ethers.HDNode.entropyToMnemonic(randomEntropyBytes);
+    }
+
+    generateRandomHDWallet() {
+        return ethers.Wallet.createRandom();
+    }
+
+    decryptWallet(json, password) {
+        return ethers.Wallet.fromEncryptedWallet(json, password);
+    }
+
+    restoreHDWallet (mnemonic) {
+        return ethers.Wallet.fromMnemonic(mnemonic);
+    }
+
+    deriveNWalletsFromHDNode(mnemonic, derivationPath, n) {
+        let wallets = [];
     
+        for (let i = 0; i < n; i++) {
+            let hdNode = ethers.HDNode.fromMnemonic(mnemonic).derivePath(derivationPath + i);
+            console.log(hdNode);
+    
+            let wallet = new ethers.Wallet(hdNode.privateKey);
+            wallets.push(wallet);
+        }
+        
+        return wallets;
+    }
+
+
+
+    sign(transactionHash, privateKey) {
+        return secp256k1.sign(new Buffer(transactionHash, 'hex'), new Buffer(privateKey, 'hex'));
+    }
+
+    verifySignature(data, publicKey, signature) {
+        // The node should verify the signature
+        let result = secp256k1.verify(
+            new Buffer(data, 'hex'), 
+            new Buffer(signature,'hex'), 
+            new Buffer(publicKey, 'hex'));
+    
+        return result;
+    }
 }
